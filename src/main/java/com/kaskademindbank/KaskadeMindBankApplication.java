345bab70cabbd3195a00539c5192b448d533df0f
Ziyuan Zhou <87697007+ZiyuanZhou901@users.noreply.github.com>
2023-11-26 15:01:30 +0800

    Initialize the project

---
+++ src/main/java/com/kaskademindbank/KaskadeMindBankApplication.java
@@ -0,0 +1,13 @@
+package com.kaskademindbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaskadeMindBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaskadeMindBankApplication.class, args);
+    }
+
+}
